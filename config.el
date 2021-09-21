bca2bb49f6eeb058dee29b059c2e41133bdc2b0c
spp <sun.april.moon@gmail.com>
2021-09-21 23:20:51 +0800

    initial commit

---
+++ config.el
@@ -0,0 +1,151 @@
+
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Pengpeng Song"
+      user-mail-address "sun.april.moon@gmail.com")
+
+;; Doom exposesf five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+ (setq doom-font (font-spec :family "PT Mono" :size 13 :weight 'semi-light)
+       doom-variable-pitch-font (font-spec :family "Consolas" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'monokai-pro-spectrum)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+(setq undo-limit 80000000)
+
+(display-time-mode 1)
+;;(global-subword-mode 1)
+
+(setq-default
+ delete-by-moving-to-trash t                      ; Delete files to trash
+ window-combination-resize t                      ; take new window space from all other windows (not just current)
+ x-stretch-cursor t)
+
+(setq +ivy-buffer-preview t)
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+
+(setq which-key-idle-delay 0.3)
+(setq iedit-toggle-key-default nil)
+
+;;(setq lsp-rust-server 'rust-analyzer)
+(use-package! lsp-mode
+  ;;:custom
+;;  (lsp-ui-doc-mode 1)
+;;  (lsp-lens-mode 1)
+  :config
+  (setq lsp-rust-server 'rust-analyzer)
+  :hook
+  (lsp-mode . lsp-ui-mode)
+  (lsp-mode . lsp-lens-mode)
+  (lsp-ui-mode . lsp-ui-doc-mode)
+  ;;  (lsp-idle-delay 0.6)
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-eldoc-render-all t)
+  ;;  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  ;; (scala-mode . lsp)
+  )
+
+;;
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;;(require 'magit)
+(xterm-mouse-mode 1)
+(global-subword-mode 1)
+
+(add-hook! 'org-mode-hook #'+org-pretty-mode #'mixed-pitch-mode)
+
+;; scala sbt and metals
+;; (use-package! scala-mode
+;;   :interpreter ("scala". scala-mode))
+
+;; (use-package! sbt-mode
+;; ;;  :command sbt-start sbt-command
+;;   :config
+;;   (substitute-key-definition
+;;    'minibuffer-complete-word
+;;    'self-insert-command
+;;    minibuffer-local-completion-map)
+
+;;   (setq sbt:program-options '("-Dsbt.supershell=false")))
+
+(use-package! clipetty
+  :hook (after-init . global-clipetty-mode)
+  )
+
+
+;;(global-clipetty-mode)
+(setq clipetty-assume-nested-mux nil)
+(setq clipetty-tmux-ssh-tty "tmux show-environment SSH_TTY")
+
+
+;; (use-package! lsp-metals
+;;  :config (setq lsp-metals-treeview-show-when-views-received))
+(unless window-system
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
+  (global-set-key (kbd "<home>") 'execute-extended-command))
+
+;;(use-package! company-lsp
+;;  :hook
+;;  (lsp-mode . (push 'company-lsp company-backends))
+;;  )
+
+;;(setq lsp-prefer-capf t)
+;;(setq lsp-completion-provider :capf)
+;;(setq lsp-completion-enable t)
+
+;;;; imenu-list
+(use-package! imenu-list
+  :commands imenu-list-smart-toggle)
+
+(after! treemacs
+  (set-popup-rule! "^ \\*Treemacs"
+    :side 'left
+    :size 0.30
+    :quit nil
+    :ttl 0))
+
+(use-package! rtags)
+(cmake-ide-setup)
+
+
+(when (daemonp)
+  (exec-path-from-shell-initialize))
---
+++ custom.el
@@ -0,0 +1,52 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["unspecified-bg" "#CC6666" "#A9DC76" "#FFD866" "#78DCE8" "#FF6188" "#78DCE8" "#FCFCFA"])
+ '(custom-safe-themes
+   '("fb83a50c80de36f23aea5919e50e1bccd565ca5bb646af95729dc8c5f926cbf3" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "1a52e224f2e09af1084db19333eb817c23bceab5e742bf93caacbfea5de6b4f6" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" default))
+ '(exwm-floating-border-color "#1c1f24")
+ '(fci-rule-color "#4C4A4D")
+ '(highlight-tail-colors
+   ((("#2c3636" "#99bb66" "green")
+     . 0)
+    (("#293b44" "#46D9FF" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#19181A" "#FCFCFA"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#19181A" "#A9DC76"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#19181A" "#727072"))
+ '(objed-cursor-color "#CC6666")
+ '(package-selected-packages '(company-lsp))
+ '(pdf-view-midnight-colors (cons "#FCFCFA" "#2D2A2E"))
+ '(rustic-ansi-faces
+   ["#2D2A2E" "#CC6666" "#A9DC76" "#FFD866" "#78DCE8" "#FF6188" "#78DCE8" "#FCFCFA"])
+ '(vc-annotate-background "#2D2A2E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A9DC76")
+    (cons 40 "#c5da70")
+    (cons 60 "#e2d96b")
+    (cons 80 "#FFD866")
+    (cons 100 "#fec266")
+    (cons 120 "#fdad66")
+    (cons 140 "#FC9867")
+    (cons 160 "#fd8572")
+    (cons 180 "#fe737d")
+    (cons 200 "#FF6188")
+    (cons 220 "#ee627c")
+    (cons 240 "#dd6471")
+    (cons 260 "#CC6666")
+    (cons 280 "#b56869")
+    (cons 300 "#9f6b6c")
+    (cons 320 "#886d6f")
+    (cons 340 "#4C4A4D")
+    (cons 360 "#4C4A4D")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
