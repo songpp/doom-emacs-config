b0ae8aaf5668fb2dfa454c32e196645ba51f4972
songpp <sun.april.moon@gmail.com>
2021-11-16 02:37:15 +0800

    Disable rtags and cmake-ide-setup by default

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
  (setq doom-font (font-spec :family "PT Mono" :size 13 :weight 'semi-light)
-       doom-variable-pitch-font (font-spec :family "Consolas" :size 13))
+       doom-variable-pitch-font (font-spec :family "Consolas for Powerline" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -143,9 +143,8 @@
     :quit nil
     :ttl 0))
 
-(use-package! rtags)
-(cmake-ide-setup)
-
+;;(use-package! rtags)
+;;(cmake-ide-setup)
 
 (when (daemonp)
   (exec-path-from-shell-initialize))
